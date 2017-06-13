6ac24b40348ed36356e021560af025f596c42cee
盧柏嘉 <lupochia@LuPaul.local>
2017-06-13 15:30:33 +0800

    renew user sing in html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery2
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.backstretch
+//= require bootstrap
+//= require scripts
+//= require app
+//= require custom
 //= require_tree .
